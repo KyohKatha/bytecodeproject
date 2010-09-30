f133f9873150918ac0b6956d5e17c024db19cacf
seilagu@gmail.com <seilagu@gmail.com@1552aba5-c92b-23e6-fd2b-bb836c3159d5>
2010-09-30 12:21:09 +0000

    Diagrama de Classes (Beans)

---
+++ trunk/TagIT/src/java/PkgTagIT/Organizador.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PkgTagIT;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 317624
+ */
+public class Organizador extends Participante{
+
+    public Organizador(double id, String email, String senha, double cpf, boolean upgrade, int tentivasUpgrade, ArrayList<Evento> evento, ArrayList<RedeSocial> redeSocial) {
+        super(id, email, senha, cpf, upgrade, tentivasUpgrade, evento, redeSocial);
+    }
+    
+}
