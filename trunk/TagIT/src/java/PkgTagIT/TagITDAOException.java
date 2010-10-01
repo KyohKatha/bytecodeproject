adf2e91c0c08ec63dd119228f9e82972ee40c6a3
seilagu@gmail.com <seilagu@gmail.com@1552aba5-c92b-23e6-fd2b-bb836c3159d5>
2010-10-01 01:15:16 +0000

    ConexaoBD e DAOExceção e driver jdbc

---
+++ trunk/TagIT/src/java/PkgTagIT/TagITDAOException.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PkgTagIT;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class TagITDAOException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public TagITDAOException() {
+    }
+
+    public TagITDAOException(String arg) {
+        super(arg);
+    }
+
+    public TagITDAOException(Throwable arg) {
+        super(arg);
+    }
+
+    public TagITDAOException(String arg, Throwable arg1) {
+        super(arg, arg1);
+    }
+}
